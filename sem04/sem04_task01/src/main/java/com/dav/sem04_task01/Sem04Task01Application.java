e69ebe9fffc6620bd56b388e8c80c4525953ac4c
EDavchick <eledavchik@gmail.com>
2025-06-29 15:27:06 +0400

    add changes in sem04

---
+++ sem04/sem04_task01/src/main/java/com/dav/sem04_task01/Sem04Task01Application.java
@@ -0,0 +1,13 @@
+package com.dav.sem04_task01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem04Task01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem04Task01Application.class, args);
+	}
+
+}
---
+++ sem04/sem04_task01/src/test/java/com/dav/sem04_task01/Sem04Task01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dav.sem04_task01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem04Task01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
