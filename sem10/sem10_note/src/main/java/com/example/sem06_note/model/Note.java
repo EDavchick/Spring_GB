0a6922442ca84d9d47f4ec1c91262078c2d0d40c
ED <154407386+EDavchick@users.noreply.github.com>
2025-10-14 16:20:28 +0400

    do not use this file

---
+++ sem10/sem10_note/src/main/java/com/example/sem06_note/model/Note.java
@@ -0,0 +1,17 @@
+package com.example.sem10_note.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@AllArgsConstructor
+@Data
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Lob
+    private String content;
+}
---
+++ sem10/sem10_note/src/main/java/com/example/sem06_note/service/NoteService.java
@@ -0,0 +1,23 @@
+package com.example.sem10_note.service;
+
+
+import com.example.sem06_note.repository.NoteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    public List<Note> findAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    public Note getNoteById(Long id) {
+        return noteRepository.findById(id);
+    }
+
+}
