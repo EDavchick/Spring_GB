31917d8443e668ee8f9feac9c6ad0f25dadcf175
ED <154407386+EDavchick@users.noreply.github.com>
2025-10-14 16:19:49 +0400

    add new exercise note for sem10

---
+++ sem10/sem10_note_exercise01/src/main/java/com/example/sem10_note_exercise01/Sem10NoteExercise01Application.java
@@ -0,0 +1,13 @@
+package com.example.sem10_note_exercise01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem10NoteExercise01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem10NoteExercise01Application.class, args);
+	}
+
+}
---
+++ sem10/sem10_note_exercise01/src/test/java/com/example/sem10_note_exercise01/Sem10NoteExercise01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem10_note_exercise01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem10NoteExercise01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
