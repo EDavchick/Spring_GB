8125642d3588655ce0c60ea62aab3d47edfd773c
ED <154407386+EDavchick@users.noreply.github.com>
2025-10-14 16:25:19 +0400

    do not use this file

--- sem10/sem10_note/src/main/java/com/example/sem06_note/controller/NoteController.java
+++ sem10/sem10_note/src/main/java/com/example/sem06_note/controller/NoteController.java
@@ -1,7 +1,7 @@
-package com.example.sem06_note.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class NoteController {
-}
+//package com.example.sem06_note.controller;
+//
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class NoteController {
+//}
--- sem10/sem10_note/src/main/java/com/example/sem06_note/repository/NoteRepository.java
+++ sem10/sem10_note/src/main/java/com/example/sem06_note/repository/NoteRepository.java
@@ -1,9 +1,9 @@
-package com.example.sem06_note.repository;
-
-import com.example.sem06_note.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
+//package com.example.sem06_note.repository;
+//
+//import com.example.sem06_note.model.Note;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface NoteRepository extends JpaRepository<Note, Long> {
+//}
